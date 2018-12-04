d8ae08d33bb4df465e29adea94aaf7d828eaaa82
NOGGONG_DAYOFF <noggong@NOGGONG-DAYOFFui-MacBookPro.local>
2018-12-05 01:31:41 +0900

    connect mysql

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 )
@@ -15,6 +16,22 @@ func main() {
 	//log.Printf("Server starting on port %v\n", port)
 	//log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 
+	//db, err := sql.Open("mysql", "noggong:123456@tcp(localhost:3306)/GoTodo")
+
+	//if err != nil {
+	//	panic(err.Error())
+	//}
+
+	//insert, err := db.Query("insert into todos values (3, 'third', now());")
+
+	//if err != nil {
+	//	panic(err.Error())
+	//}
+
+
+
 	router := NewRouter()
 	log.Fatal(http.ListenAndServe(":4040", router))
+
+	//defer insert.Close()
 }
